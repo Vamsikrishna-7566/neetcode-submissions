dae59ab79e52013268b459a0f5b135bf195672ab
Naga Padma Vamsi Krishna Divvela <114364267+Vamsikrishna-7566@users.noreply.github.com>
2026-09-21 16:16:21 -0400

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+
+        // Find a meeting point inside the cycle.
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Find the cycle entrance.
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
